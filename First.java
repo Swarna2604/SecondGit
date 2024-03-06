734803256f52911f0707ff5dae371ee00b3f7e2b
Swarna2604 <159441343+Swarna2604@users.noreply.github.com>
2024-03-06 12:50:53 +0530

    Update First.java

--- First.java
+++ First.java
@@ -2,7 +2,6 @@ class First{
 public static void main(String a[])
 {
  System.out.println("Welcome to Java");
- System.out.println("Welcome to FirstBranch");
- 
+ System.out.println("Welcome to FirstBranch"); 
 }
 }
